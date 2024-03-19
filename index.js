bdc8f254a61f7f73b10acf53d8177feaee1ff96e
Sadi Musa <sadimusa00@gmail.com>
2024-03-19 13:01:19 +0300

    done

--- index.js
+++ index.js
@@ -9,3 +9,44 @@ function ternaryCheckCity(){
 function switchOnCharmFromTip(){
   // Write your code here!
 }
+// Import necessary helper functions if any
+const helpers = require('./helpers');
+
+// Define scuberGreetingForFeet function
+function scuberGreetingForFeet(feet) {
+  let message;
+  if (feet <= 400) {
+    message = 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+    message = 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+    message = 'I will gladly take your thirty bucks.';
+  } else {
+    message = 'No can do.';
+  }
+  return message;
+}
+
+// Define ternaryCheckCity function
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+// Define switchOnCharmFromTip function
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
+
+// Export the functions
+module.exports = {
+  scuberGreetingForFeet,
+  ternaryCheckCity,
+  switchOnCharmFromTip
+};
